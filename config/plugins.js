dceaf80e9d67bbb54da3b530b451cd1d5d22eadb
김예림 <yrgim@marketboro.com>
2024-04-26 12:25:16 +0900

    server.js 추가

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,14 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  "users-permissions": {
+    config: {
+      grant: {
+        google: {
+          redirectUri: env(
+            "GOOGLE_REDIRECT_URI",
+            "http://localhost:3000/login"
+          ),
+        },
+      },
+    },
+  },
+});
--- config/server.js
+++ config/server.js
@@ -7,5 +7,5 @@ module.exports = ({ env }) => ({
   webhooks: {
     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
-  // url: "https://mybackend.emogeediary.site",
+  proxy: true,
 });
